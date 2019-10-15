358ee5455ae6a42704db98604315bcac4cab08a6
Danny Chan <dchan6513@gmail.com>
2019-10-15 15:17:07 -0400

    Add logic to pressing the help buttons

--- src/index.js
+++ src/index.js
@@ -1,31 +1,60 @@
 import './styles/index.scss';
 
 window.addEventListener('DOMContentLoaded', () => {
-  // document.getElementById('app').innerText = "Hello World!";
   document.getElementById('webpage');
 });
 
-const helpScreen = document.getElementById('help-screen');
-const myCanvas = document.getElementById('myCanvas');
+
+// Get the divs holding instructions and the periodic table
+const instructionScreen = document.getElementById('instruction-screen');
+const periodicTable = document.getElementById('periodic-table');
+
+
+// Get the canvas
+const canvas = document.getElementById('canvas');
 const ctx = canvas.getContext('2d');
 
-const helpButton = document.getElementById('help-button');
-const periodicButton = document.getElementById('periodic-button');
+// const helpButton = document.getElementById('help-button');
+// const periodicButton = document.getElementById('periodic-button');
 
-function handleHelp() {
-  helpScreen.className = 'show';
-  canvas.className = 'hide';
-};
 
-function returnToCanvas(e) {
-  if (e.key === "Escape") {
-    helpScreen.className = 'hide';
-    canvas.className = 'show';
+// Add event listeners to the help buttons
+const helpButtons = document.getElementsByClassName('help-button');
+for (let i = 0; i < helpButtons.length; i++) {
+  helpButtons[i].addEventListener('click', handleHelpButton, false);
+}
+
+function handleHelpButton(e) {
+  debugger
+  if (e.target.id === 'instruction-button') {
+    instructionScreen.className = 'show';
+    periodicTable.className = 'hide';
+    canvas.className = 'hide';
+  } else if (e.target.id === 'periodic-button') {
+    instructionScreen.className = 'hide';
+    periodicTable.className = 'show';
+    canvas.className = 'hide';
+  } else {
+    returnToCanvas();
   }
 };
 
-helpButton.addEventListener('click', handleHelp);
-window.addEventListener('keydown', returnToCanvas)
+function returnToCanvas() {
+  // if (e.key === "Escape") {
+    instructionScreen.className = 'hide';
+    periodicTable.className = 'hide';
+    canvas.className = 'show';
+  // }
+};
+
+function handleKeyPress(e) {
+  if (e.key === "Escape") {
+    returnToCanvas();
+  }
+}
+
+// helpButton.addEventListener('click', handleHelp);
+window.addEventListener('keydown', handleKeyPress)
 
 // Figuring out how to use event listeners on DOM elements
 // helpButton.addEventListener('click', function() { alert("I clicked the help button"); });
