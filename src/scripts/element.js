0cda0f26243e1731c80c9dd5497e966abd1d42fd
Danny Chan <dchan6513@gmail.com>
2019-10-16 09:44:49 -0400

    Install periodic-table package and test its usage

---
+++ src/scripts/element.js
@@ -0,0 +1,7 @@
+// Element data is sourced from the periodic-table package
+// https://www.npmjs.com/package/periodic-table
+
+const periodicTable = require('periodic-table');
+
+const helium = periodicTable.elements.Helium;
+console.log(helium);
