adb5d68db4d6caaa3a67523ee16c8355957d32e2
Danny Chan <dchan6513@gmail.com>
2019-10-15 21:57:29 -0400

    Refactor toggling classes in game div

--- src/index.js
+++ src/index.js
@@ -6,14 +6,18 @@ window.addEventListener('DOMContentLoaded', () => {
 });
 
 // Get the divs holding instructions and the periodic table
-const instructionScreen = document.getElementById('instruction-screen');
+const instructionsScreen = document.getElementById('instructions-screen');
+const controlsScreen = document.getElementById('controls-screen');
 const periodicTable = document.getElementById('periodic-table');
+
+const gameDivs = document.getElementById('game').children;
+console.log(gameDivs);
 
 // Get the canvas
 const canvas = document.getElementById('canvas');
 const ctx = canvas.getContext('2d');
 
-canvas.width = 640;
+canvas.width = 800;
 canvas.height = 480;
 
 const player = new Player(canvas, ctx);
@@ -26,22 +30,53 @@ for (let i = 0; i < helpButtons.length; i++) {
 
 // Upon pressing one of the help buttons, show the relevant display
 function handleHelpButton(e) {
-  if (e.target.id === 'instruction-button') {
-    instructionScreen.className = 'show';
-    periodicTable.className = 'hide';
-    canvas.className = 'hide';
-  } else if (e.target.id === 'periodic-button') {
-    instructionScreen.className = 'hide';
-    periodicTable.className = 'show';
-    canvas.className = 'hide';
-  } else {
-    returnToCanvas();
+  let showDivId;
+  switch (e.target.id) {
+    case 'about-button':
+      showDivId = 'instructions-screen';
+      break;
+    case 'controls-button':
+      showDivId = 'controls-screen';
+      break;
+    case 'periodic-button':
+      showDivId = 'periodic-table';
+      break;
+    default:
+      showDivId = 'canvas';
   }
+
+  for (let i = 0; i < gameDivs.length; i++ ) {
+    if (gameDivs[i].id === showDivId) {
+      gameDivs[i].className = 'show';
+    }
+    else {
+      gameDivs[i].className = 'hide';
+    }
+  }
+  // if (e.target.id === 'instruction-button') {
+  //   instructionScreen.className = 'show';
+  //   periodicTable.className = 'hide';
+  //   canvas.className = 'hide';
+  // } 
+  // else if (e.target.id ===)
+  // else if (e.target.id === 'periodic-button') {
+  //   instructionScreen.className = 'hide';
+  //   periodicTable.className = 'show';
+  //   canvas.className = 'hide';
+  // } 
+  // else {
+  //   returnToCanvas();
+  // }
+
+  // switch (e.target.id) {
+  //   case 'instruction-button'
+  // }
 };
 
 // Return to the game
 function returnToCanvas() {
-    instructionScreen.className = 'hide';
+    instructionsScreen.className = 'hide';
+    controlsScreen.className = 'hide';
     periodicTable.className = 'hide';
     canvas.className = 'show';
 };
@@ -67,7 +102,7 @@ function draw() {
   player.drawPlayer();
 }
 
-setInterval(draw, 5);
+setInterval(draw, 10);
 
 // Figuring out how to use event listeners on DOM elements
 // helpButton.addEventListener('click', function() { alert("I clicked the help button"); });
