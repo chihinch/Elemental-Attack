d57ab1c5eeb360b5a2787c98c0356c495bebead7
Danny Chan <dchan6513@gmail.com>
2019-10-17 16:27:38 -0400

    Format oxidation state as superscript

--- src/scripts/atom.js
+++ src/scripts/atom.js
@@ -54,7 +54,8 @@ export default class Atom {
     this.ctx.fillText(this.symbol, this.positionX, this.positionY, this.radius);
     this.ctx.beginPath();
     // this.textAlign = 'right';
-    this.ctx.fillText(oxidationStateFormat, this.positionX + fontSize, this.positionY - fontSize);
+    this.ctx.font = `${fontSize * 0.75}px Arial`
+    this.ctx.fillText(oxidationStateFormat, this.positionX + (fontSize * 0.75), this.positionY - (fontSize * 0.75));
     this.ctx.closePath();
   }
 
