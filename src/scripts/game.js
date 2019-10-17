1b44274e9fd370f723ef72ba983b86e121abd2b3
Danny Chan <dchan6513@gmail.com>
2019-10-16 22:26:45 -0400

    Refactor index.js to move its game functionality to game.js

--- src/scripts/game.js
+++ src/scripts/game.js
@@ -1,15 +1,33 @@
 import Player from './player';
-import Element from './atom';
+import Atom from './atom';
 
 export default class Game {
   constructor(canvas, ctx) {
+    // Things to draw on
     this.canvas = canvas;
     this.ctx = ctx;
-    this.player = new Player(this.canvas, this.ctx);
+    // this.player = new Player(this.canvas, this.ctx);
 
+    // Game status
     this.paused = false;
     this.over = false;
     this.score = 0;
+
+    // Entities being drawn
+    this.entities = {};
+
+    // Render the game
+    this.renderGame = this.renderGame.bind(this);
+  }
+
+  renderGame() {
+    // Return nothing if the game is paused
+    if (this.paused) {
+      return;
+    }
+
+    // Clear the canvas for the next frame
+    this.ctx.clearRect(0, 0, this.canvas.width, this.canvas.height);
   }
 
 }
