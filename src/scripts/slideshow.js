8d15e4e5416922d5cefbc93d663995284b332a20
Danny Chan <dchan6513@gmail.com>
2019-12-03 23:28:30 -0500

    Add navbar colours to indicate slideshow progress

--- src/scripts/slideshow.js
+++ src/scripts/slideshow.js
@@ -13,7 +13,7 @@ export default class Slideshow {
     this.instructions = new Image();
     this.instructions.src = "/src/assets/images/aboutSlideshow/elementalAttackDiagram.png";
 
-    this.slideNumber = 1;
+    this.slideNumber = 3;
     this.allowGameStart = false;
 
     this.clearCanvas = this.clearCanvas.bind(this);
@@ -21,6 +21,9 @@ export default class Slideshow {
     this.drawStory = this.drawStory.bind(this);
     this.drawInstructions = this.drawInstructions.bind(this);
     this.drawNavbar = this.drawNavbar.bind(this);
+    this.drawNavBarOne = this.drawNavBarOne.bind(this);
+    this.drawNavBarTwo = this.drawNavBarTwo.bind(this);
+    this.drawNavBarThree = this.drawNavBarThree.bind(this);
     this.drawSlide = this.drawSlide.bind(this);
     this.advanceSlide = this.advanceSlide.bind(this);
     this.backwardSlide = this.backwardSlide.bind(this);
@@ -45,23 +48,62 @@ export default class Slideshow {
   }
 
   drawNavbar() {
-    const currentColor = "#9bf542";
-    const otherColor = "#ffffff";
-
     this.ctx.beginPath();
-      this.ctx.rect(0, 575, 1200, 25);
+      this.ctx.rect(0, 570, 1200, 30);
       this.ctx.fillStyle = "black";
-      this.ctx.fill();
-      this.ctx.stroke();
-    this.ctx.closePath();
-      
+    this.ctx.fill();
+
+    switch (this.slideNumber) {
+      case 1:
+        this.drawNavBarOne();
+        break;
+      case 2:
+        this.drawNavBarOne();
+        this.drawNavBarTwo();
+        break;
+      case 3:
+        this.drawNavBarOne();
+        this.drawNavBarTwo();
+        this.drawNavBarThree();
+        break;
+      default: 
+        return;
+    }
+  }
+
+  drawNavBarOne() {
     this.ctx.beginPath();
-      this.ctx.strokeStyle = currentColor;
-      this.ctx.fillStyle = currentColor;
-      this.ctx.arc(300, 588, 10, 0, 2 * Math.PI, true);
-      this.ctx.fill();
-      this.ctx.stroke();
-    this.ctx.closePath();
+      this.ctx.rect(0, 570, 400, 30);
+      this.ctx.moveTo(400, 570);
+      this.ctx.lineTo(425, 585);
+      this.ctx.lineTo(400, 600);
+      this.ctx.fillStyle = "#e60909";
+    this.ctx.fill();
+  }
+
+  drawNavBarTwo() {
+    this.ctx.beginPath();
+      this.ctx.moveTo(400, 570);
+      this.ctx.lineTo(800, 570);
+      this.ctx.lineTo(825, 585);
+      this.ctx.lineTo(800, 600);
+      this.ctx.lineTo(400, 600);
+      this.ctx.lineTo(425, 585);
+      this.ctx.lineTo(400, 570);
+      this.ctx.fillStyle = "#e6e609";
+    this.ctx.fill();
+  }
+
+  drawNavBarThree() {
+    this.ctx.beginPath();
+      this.ctx.moveTo(800, 570);
+      this.ctx.lineTo(1200, 570);
+      this.ctx.lineTo(1200, 600);
+      this.ctx.lineTo(800, 600);
+      this.ctx.lineTo(825, 585);      
+      this.ctx.lineTo(800, 570);      
+      this.ctx.fillStyle = "#26e609";
+    this.ctx.fill();
   }
 
   drawSlide() {
